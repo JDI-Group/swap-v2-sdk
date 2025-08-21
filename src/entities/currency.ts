2be62fc6925e602cfff4d6099bd558b18f578984
hairyf <wwu710632@gmail.com>
2025-08-21 13:18:13 +0800

    fix: modify the symbol in the currency instance from 'MXC' to 'MCH'

--- src/entities/currency.ts
+++ src/entities/currency.ts
@@ -16,7 +16,7 @@ export class Currency {
   /**
    * The only instance of the base class `Currency`.
    */
-  public static readonly ETHER: Currency = new Currency(18, 'MXC', 'MXC')
+  public static readonly ETHER: Currency = new Currency(18, 'MCH', 'MCH')
 
   /**
    * Constructs an instance of the base class `Currency`. The only instance of the base class `Currency` is `Currency.ETHER`.
